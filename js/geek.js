c8f1e349aa439ddefbba24e786acc58aa7aa522a
SWakky <102840595+SWakky@users.noreply.github.com>
2022-04-21 09:15:50 +0900

    4/21アップ

--- js/geek.js
+++ js/geek.js
@@ -1,4 +1,4 @@
-const SIZE = 131; // 枚数
+const SIZE = 211; // 枚数
 var tmp = {};
     function loadImageToTmp(){
         for(var i=1;i<=SIZE;i++){
@@ -13,17 +13,27 @@ var tmp = {};
     }
 console.log("画像プリロード完了")
 
-const PX = 5; // 5px分の移動ごと画像を1枚進める   
+loadImageToTmp();
+const image = document.getElementById("anim_img");
+console.log(image);//とった文の確かめ用
+
+const PX = 20; // 10px分の移動ごと画像を1枚進める   
 const offset = $("#anim_img_box").offset(); // 画像を入れるdiv要素(position:stickyでトップに来たら固定される)
     $(window).scroll(function() {
         const y = $(window).scrollTop();
         const dy = y - offset.top;
+        console.log( y - offset.top); //ちゃんと指定（#anim_img_box）offsetからｙ軸の計算できてるか？
+        console.log(offset.top + SIZE*PX); //offset.topにスクロール分の画像枚数とスクロールPX足せているか？
         if(offset.top<y&&y<offset.top+SIZE*PX){
             $("#anim_img_box").css("top",0)
             const i = Math.floor(dy / PX);
-            if(i<=0||i>=SIZE) return;
+            if(i<=0||i>=SIZE) return; //ここはスクロールバックのこと言ってる
             if(tmp[i].src) image.src = tmp[i].src;
-            }else if(y>=offset.top+SIZE*PX){
-                $("#anim_img_box").css("top","-"+(dy-SIZE*PX)); // スクロール分が終了したときに移動を始める
-            }
+            console.log("top0確認")
+        }else if(y>=offset.top+SIZE*PX){
+            $("#anim_img_box").css("top","-"+(dy-SIZE*PX)); // スクロール分が終了したときに移動を始める
+            console.log(dy-SIZE*PX)
+        }
         });
+
+        $("#anim_img_padding").height(SIZE*PX);
